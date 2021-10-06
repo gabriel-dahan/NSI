cc5bcdc14918d7a2b0af6fe037cf0f022a08fbb6
thegabdoosan <gabrieldahan05@gmail.com>
2021-10-06 13:29:35 +0200

    Release

--- python/dm/chifoumi.py
+++ python/dm/chifoumi.py
@@ -14,18 +14,18 @@ import random
 
 def partie_simple():
     j1 = input('Pierre, Feuille ou Ciseaux (p, f ou c) ? ')
-    while not j1 or j1 not in 'pfc':
+    while j1 not in 'pfc':
         j1 = input('p, f ou c : ')
     j2 = random.choice('pfc')
     if j1 == j2:
-        winner = 0
+        gagnant = 0
     elif (j1 == 'p' and j2 == 'c') \
         or (j1 == 'f' and j2 == 'p') \
         or (j1 == 'c' and j2 == 'f'):
-        winner = 1
+        gagnant = 1
     else:
-        winner = 2
-    return winner
+        gagnant = 2
+    return gagnant
 
 if __name__ == '__main__':
     score_j1 = 0
@@ -33,14 +33,14 @@ if __name__ == '__main__':
     for _ in range(10):
         if score_j1 == 5 or score_j2 == 5:
             break
-        winner = partie_simple()
-        if winner == 0:
+        gagnant = partie_simple()
+        if gagnant == 0:
             print(f'Match nul.')
-        elif winner == 1:
-            print(f'Vous avez gagné !')
+        elif gagnant == 1:
+            print(f'J1 a gagné la manche.')
             score_j1 += 1
         else:
-            print(f'L\'ordinateur à gagné.')
+            print(f'J2 a gagné la manche.')
             score_j2 += 1
         print(f'\n--> Manches gagnées par J1 : {score_j1}/10')
         print(f'--> Manches gagnées par J2 : {score_j2}/10')
