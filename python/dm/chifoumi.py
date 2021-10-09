6495766e272c826b7891ef8daf3f3560c9a4a86e
thegabdoosan <gabrieldahan05@gmail.com>
2021-10-09 18:52:46 +0200

    Release

--- python/dm/chifoumi.py
+++ python/dm/chifoumi.py
@@ -1,3 +1,4 @@
+# REPO FILE : https://github.com/gabriel-dahan/NSI/blob/main/python/dm/chifoumi.py
 """
     Pierre-Feuille-Ciseau (ou Chifoumi) est un jeu où 2 joueurs s'affrontent en choisissant chacun Pierre, Feuille ou Ciseaux.
     La Pierre écrase les Ciseaux, la Feuille enveloppe la Pierre, les Ciseaux découpent la Feuille.
@@ -13,48 +14,48 @@
 import random
 from typing import Literal
 
-def partie_simple() -> Literal[0, 1, 2]:
-    """ Renvoie un entier spécifiant le résultat de la partie. """
-    j1 = input('Pierre, Feuille ou Ciseaux (p, f ou c) ? ')
-    while j1 not in 'pfc':
-        j1 = input('p, f ou c : ')
-    j2 = random.choice('pfc')
-    if j1 == j2:
+def simple_round() -> Literal[0, 1, 2]:
+    """ Returns an integer specifying the result of the part. """
+    p1 = input('Rock, Paper or Scissors (r, p or s) ? ')
+    while p1 not in 'rps':
+        p1 = input('r, p or s : ')
+    p2 = random.choice('rps')
+    if p1 == p2:
         gagnant = 0
-    elif (j1 == 'p' and j2 == 'c') \
-        or (j1 == 'f' and j2 == 'p') \
-        or (j1 == 'c' and j2 == 'f'):
+    elif (p1 == 'r' and p2 == 's') \
+        or (p1 == 'p' and p2 == 'r') \
+        or (p1 == 's' and p2 == 'p'):
         gagnant = 1
     else:
         gagnant = 2
     return gagnant
 
-def pfc(m: int) -> None:
-    """ Joue et affiche les résultats d'une partie de m manches. """
-    assert m >= 1, 'Une partie doit compter au moins 1 manche.'
-    score_j1 = 0
-    score_j2 = 0
-    max_score = m / 2
+def play_chifoumi(rounds: int = 10) -> None:
+    """ Play and display the results of a game of m rounds. """
+    assert rounds >= 1, 'A game must last at least 1 round.'
+    p1_score = 0
+    p2_score = 0
+    max_score = rounds // 2 if rounds % 2 == 0 else (rounds // 2) + 1
     i = 0
-    while i < m and (score_j1 < max_score and score_j2 < max_score):
-        gagnant = partie_simple()
+    while i < rounds and (p1_score < max_score and p2_score < max_score):
+        gagnant = simple_round()
         if gagnant == 0:
-            print('Match nul.')
+            print('Draw...')
         elif gagnant == 1:
-            print('J1 a gagné la manche.')
-            score_j1 += 1
+            print('P1 won the round.')
+            p1_score += 1
         else:
-            print('J2 a gagné la manche.')
-            score_j2 += 1
-        print(f'\n--> Manches gagnées par J1 : {score_j1}/{max_score}')
-        print(f'--> Manches gagnées par J2 : {score_j2}/{max_score}')
+            print('P2 won the round.')
+            p2_score += 1
+        print(f'\n--> Rounds won by P1 : {p1_score}/{max_score}')
+        print(f'--> Rounds won by P2 : {p2_score}/{max_score}')
         i += 1
-    if score_j1 > score_j2:
-        print(f'\n### J1 gagne la partie avec {score_j1} points contre {score_j2} ! ###')
-    elif score_j1 == score_j2:
-        print(f'\n### Match nul, J1 a autant de points que J2. ###')
+    if p1_score > p2_score:
+        print(f'\n### P1 wins the game with {p1_score} points against {p2_score} ! ###')
+    elif p1_score == p2_score:
+        print(f'\n### Draw, P1 has the same score as P2. ###')
     else:
-        print(f'\n### J2 gagne la partie avec {score_j2} points contre {score_j1} ! ###')
+        print(f'\n### P2 wins the game with {p2_score} points against {p1_score} ! ###')
 
 if __name__ == '__main__':
-    pfc()
+    play_chifoumi()
