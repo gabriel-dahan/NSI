684b7649a1c1c033c71f8cede125208657cf35e5
thegabdoosan <gabrieldahan05@gmail.com>
2021-10-27 15:02:59 +0200

    Releas

--- python/dm/money_change.py
+++ python/dm/money_change.py
@@ -2,6 +2,10 @@ def money_change(asked: int, paid: int, cash_register: list) -> list:
     """ Returns a list of the number of each type of currency the cashier must return. """
     assert paid >= asked, 'You don\'t have enough money to pay.'
     rest = paid - asked
+    total_cash = 0
+    for currency in cash_register:
+        total_cash += currency[0] * currency[1]
+    assert total_cash >= rest, 'The cash register does not contain enough money to return.'
     currencies = [
         [50, 0],
         [20, 0],
@@ -13,10 +17,10 @@ def money_change(asked: int, paid: int, cash_register: list) -> list:
     for i, ctype in enumerate(currencies):
         running = True
         while running and rest >= ctype[0]:
-            if cash_register[i] - 1 < 0:
+            if cash_register[i][1] - 1 < 0:
                 running = False
                 continue
-            cash_register[i] -= 1
+            cash_register[i][1] -= 1
             ctype[1] += 1
             rest -= ctype[0]
     return currencies
@@ -31,8 +35,13 @@ def format_change(currencies: list) -> None:
         change += f'\n - {currency[1]} x {currency[0]}$'
     print(f'Change to be returned : {change}')
 
-def main(asked: int, paid: int):
-    format_change(money_change(asked, paid))
+def main(asked: int, paid: int, cash_register: list, no_format: bool = False) -> None:
+    """ Launches the program. """
+    change = money_change(asked, paid, cash_register)
+    if no_format:
+        print(change)
+        return
+    format_change(change)
 
 if __name__ == '__main__':
     nb_1euro = 15
@@ -41,12 +50,12 @@ if __name__ == '__main__':
     nb_10euros = 10
     nb_20euros = 5
     nb_50euros = 4
-    cash_register = [
-        nb_50euros,
-        nb_20euros,
-        nb_10euros,
-        nb_5euros,
-        nb_2euros,
-        nb_1euro
-    ]
-    format_change(money_change(12, 455, cash_register))
+
+    main(12, 50, [
+        [50, nb_50euros],
+        [20, nb_20euros],
+        [10, nb_10euros],
+        [5, nb_5euros],
+        [2, nb_2euros],
+        [1, nb_1euro]
+    ])
