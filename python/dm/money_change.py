d5d86e4401f5818a17714335f3ac317fe952e370
thegabdoosan <gabrieldahan05@gmail.com>
2021-10-27 14:41:22 +0200

    Release

--- python/dm/money_change.py
+++ python/dm/money_change.py
@@ -1,15 +1,8 @@
-nb_50euros = 0
-nb_20euros = 0
-nb_10euros = 0
-nb_5euros  = 0
-nb_2euros  = 0
-nb_1euro   = 0
-
-def money_change(asked: int, paid: int) -> list:
+def money_change(asked: int, paid: int, cash_register: list) -> list:
     """ Returns a list of the number of each type of currency the cashier must return. """
     assert paid >= asked, 'You don\'t have enough money to pay.'
     rest = paid - asked
-    currency = [
+    currencies = [
         [50, 0],
         [20, 0],
         [10, 0],
@@ -17,21 +10,43 @@ def money_change(asked: int, paid: int) -> list:
         [2, 0],
         [1, 0]
     ]
-    for ctype in currency:
-        while rest >= ctype[0]:
+    for i, ctype in enumerate(currencies):
+        running = True
+        while running and rest >= ctype[0]:
+            if cash_register[i] - 1 < 0:
+                running = False
+                continue
+            cash_register[i] -= 1
             ctype[1] += 1
             rest -= ctype[0]
-    return currency
+    return currencies
 
-def format_change(currency: list) -> None:
+def format_change(currencies: list) -> None:
     """ Prints the change to be returned by the cashier. """
-    # change = '\n - ' + '\n - '.join([f"{elem[0]}$ --> {elem[1]}" for elem in currency if elem[1] > 0])
+    # change = '\n - ' + '\n - '.join([f"{currency[1]} x {currency[0]}$" for currency in currencies if currency[1] > 0])
     change = ''
-    for elem in currency:
-        if not elem[1] > 0:
+    for currency in currencies:
+        if not currency[1] > 0:
             continue
-        change += f'\n - {elem[0]}$ --> {elem[1]}'
+        change += f'\n - {currency[1]} x {currency[0]}$'
     print(f'Change to be returned : {change}')
 
+def main(asked: int, paid: int):
+    format_change(money_change(asked, paid))
+
 if __name__ == '__main__':
-    format_change(money_change(12, 50))
+    nb_1euro = 15
+    nb_2euros = 4
+    nb_5euros = 4
+    nb_10euros = 10
+    nb_20euros = 5
+    nb_50euros = 4
+    cash_register = [
+        nb_50euros,
+        nb_20euros,
+        nb_10euros,
+        nb_5euros,
+        nb_2euros,
+        nb_1euro
+    ]
+    format_change(money_change(12, 455, cash_register))
